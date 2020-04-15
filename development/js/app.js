b28ea70b2c676a819101df6b858e026fe44c729e
Agata Urbańska <agata.urbanska9@gmail.com>
2020-04-15 13:54:01 +0200

    form visibility broken

--- development/js/app.js
+++ development/js/app.js
@@ -1,28 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    //sprawdź czy już istnieje imię!!!
-
     let btnSubmit = document.querySelector('.first_entry_btn');
     let name = document.querySelector('input[value][type="text"]');
     let errorMsg = document.querySelector('.error_msg');
-    // let form = document.querySelector('.first_entry_form');
+    let nameSpan = document.querySelector('.header__name span');
+    let form = document.querySelector('.first_entry_form');
+
+        //jeżeli w local storage zapisane zostało imię pozostaw formularz niewidoczny i zmień imię w navie
+        if (localStorage.getItem('name')!=null) {
+            nameSpan.innerHTML = localStorage.getItem('name');
+        } else {
+            //odkryj formularz
+            form.classList.remove('.hide');
+        }
 
     btnSubmit.addEventListener('click', function (e) {
         e.preventDefault();
         if (name.value.length < 3) {
-            errorMsg.classList.add('visible');
-        } else {
             errorMsg.classList.remove('visible');
-            //zapisz w lokal storage
+        } else {
+            errorMsg.classList.add('visible');
+            //zapisz imię w local storage
             let userName = name.value;
             localStorage.setItem("name", userName);
-            //zmień value
-            let nameSpan = document.querySelector('.header__name span');
+            //zmień imię w navie
             nameSpan.innerHTML = localStorage.getItem('name');
-            //daj invisible na formularz
-
+            //ukryj formularz
+            form.classList.add('.hide');
         }
-
     });
-
 });
