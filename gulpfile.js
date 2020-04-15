b92f1356eb37fd877723e5e0a4a618750c73915f
Patryk Urbanski <pat.urbanski@gmail.com>
2020-04-15 10:33:39 +0200

    correct ver of master, fixed font and bg

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,6 @@ gulp.task("watch", function(cb) {
 gulp.task("serve", function(cb) {
   browserSync.init({
     server: "./development",
-    browser: "chrome"
   });
   gulp.watch("development/scss/**/*.scss", gulp.series("sass"));
   gulp.watch("development/*.html").on("change", browserSync.reload);
