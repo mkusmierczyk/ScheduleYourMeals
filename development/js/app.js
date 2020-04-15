4da58d61bbc7007c43d95e456b035810390b9da5
Agata Urbańska <agata.urbanska9@gmail.com>
2020-04-15 14:20:48 +0200

    js entry form done

--- development/js/app.js
+++ development/js/app.js
@@ -6,27 +6,31 @@ document.addEventListener('DOMContentLoaded', function () {
     let nameSpan = document.querySelector('.header__name span');
     let form = document.querySelector('.first_entry_form');
 
+    form.classList.add('hide');
+
         //jeżeli w local storage zapisane zostało imię pozostaw formularz niewidoczny i zmień imię w navie
         if (localStorage.getItem('name')!=null) {
             nameSpan.innerHTML = localStorage.getItem('name');
+            console.log(localStorage.name);
         } else {
             //odkryj formularz
-            form.classList.remove('.hide');
+            form.classList.remove('hide');
         }
+        errorMsg.classList.add('visible');
 
-    btnSubmit.addEventListener('click', function (e) {
-        e.preventDefault();
-        if (name.value.length < 3) {
-            errorMsg.classList.remove('visible');
-        } else {
-            errorMsg.classList.add('visible');
-            //zapisz imię w local storage
-            let userName = name.value;
-            localStorage.setItem("name", userName);
-            //zmień imię w navie
-            nameSpan.innerHTML = localStorage.getItem('name');
-            //ukryj formularz
-            form.classList.add('.hide');
-        }
-    });
+        btnSubmit.addEventListener('click', function (e) {
+            e.preventDefault();
+            if (name.value.length < 3) {
+                errorMsg.classList.remove('visible');
+            } else {
+                errorMsg.classList.toggle('visible');
+                //zapisz imię w local storage
+                let userName = name.value;
+                localStorage.setItem("name", userName);
+                //zmień imię w navie
+                nameSpan.innerHTML = localStorage.getItem('name');
+                //ukryj formularz
+                form.classList.add('hide');
+            }
+        });
 });
