bd4e866022a28f212612937fd67325c3f6b2065f
Patryk Urbanski <pat.urbanski@gmail.com>
2020-04-16 10:05:18 +0200

    plan display layout done

---
+++ development/js/schedules_displayPlanList.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+   //new whole row
+   //new td with Id iteration
+   //new td with localstorage.planName
+   //new td with localstorage.planDescription
+   // new td with localstorage.weekNumber
+   const editBtn = document.querySelector("td i:first-child");
+   const deleteBtn = document.querySelector("td i:last-child");
+   const addBtn = document.querySelector("button i");
+
+   editBtn.addEventListener("click", function () {
+       //przejscie do edycji, wyświetleie popup edycji
+   });
+
+   deleteBtn.addEventListener("click", function () {
+        //new whole row add class hide
+
+   });
+
+   addBtn.addEventListener("click", function () {
+       //przejscie do okna dodawania nowego planu
+
+   })
+
+});
