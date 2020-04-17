219e9b774aba56e3eb691a8d9e66625a81db35b5
Agata Urbańska <agata.urbanska9@gmail.com>
2020-04-17 13:26:19 +0200

    13:26

--- development/js/newrecipe.js
+++ development/js/newrecipe.js
@@ -1,13 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const newRecipeForm = document.querySelector('.new_recipe_form');
+
     const addNewRecipeBtn = document.querySelector('.save_quit'); //btn dodaje przepis do planu i zamyka okno nowy przepis
+
     const addInstructionBtn = document.querySelector('.add_instruction');
     const addIngredientBtn = document.querySelector('.add_ingredient');
-    const instructionList = document.querySelector('.recipeInstruction-list');
-    const ingredientsList = document.querySelector('.recipeIngredients-list');
 
     const inputInstruction = document.querySelector('input[name="recipeInstructions"]');
     const inputIngredient = document.querySelector('input[name="recipeIngredients"]');
+
+    const title = document.querySelector('input[name="recipeName"]');
+    const description = document.querySelector('textarea[name="recipeDescription"]');
+    const ingredients = document.querySelector('.recipeIngredients-list');
+    const instructions = document.querySelector('.recipeInstruction-list');
+
+
+    const newRecipe = {
+        id: '',
+        title: '',
+        description: '',
+        ingredients: [],
+        instruction: []
+    };
 
     addIngredientBtn.addEventListener('click', function (e) {
         e.preventDefault();
@@ -28,14 +41,16 @@ document.addEventListener("DOMContentLoaded", function () {
             newIngredient.innerText = inputIngredient.value;
             newIngredient.appendChild(trashBtn);
             newIngredient.appendChild(editBtn);
-            ingredientsList.appendChild(newIngredient);
+            ingredients.appendChild(newIngredient);
+
+            //zapisz składnik do tablicy która jest polem obiektu newRecipe
+            newRecipe.ingredients.push(newIngredient.innerText);
 
             trashBtn.addEventListener('click', function () {
                 ingredientsList.removeChild(newIngredient);
             });
             editBtn.addEventListener('click', function () {
                 // if (newIngredient.attributes[])
-
             });
         }
     });
@@ -59,16 +74,38 @@ document.addEventListener("DOMContentLoaded", function () {
             newInstruction.innerText = inputInstruction.value;
             newInstruction.appendChild(trashBtn);
             newInstruction.appendChild(editBtn);
-            instructionList.appendChild(newInstruction);
+            instructions.appendChild(newInstruction);
+            //zapisz instrukcję do tablicy która jest polem obiektu newRecipe
+            newRecipe.instruction.push(newInstruction.innerText);
 
             trashBtn.addEventListener('click', function () {
                 instructionList.removeChild(newInstruction);
             });
             editBtn.addEventListener('click',function () {
-
-
+                //<-------
             });
         }
+    });
+
+    let saveRecipeToLS = function (newObject) {
+        let dataFromLocalStorage = [];
+        if (localStorage.getItem('newRecipe')!=null) {
+            dataFromLocalStorage = JSON.parse(localStorage.getItem('newRecipe'));
+            dataFromLocalStorage.push(newObject);
+            localStorage.setItem('recipes', JSON.stringify(dataFromLocalStorage));
+        } else {
+            dataFromLocalStorage.push(newObject);
+            localStorage.setItem('recipes', JSON.stringify(dataFromLocalStorage));
+        }
+
+    };
+    addNewRecipeBtn.addEventListener('click', function (e) {
+        e.preventDefault();
+        newRecipe.id = localStorage.recipes.length+1;
+        newRecipe.description = description.value;
+        newRecipe.title = title.value;
+        saveRecipeToLS(newRecipe);
+
 
     });
 
