e2ff56059a39582a06b5a6ccc4503f6f6d88b5cd
Mateusz <makusmierczyk@gmail.com>
2020-04-17 10:56:49 +0200

    IFrame done

---
+++ development/js/plan_choice.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const prevBtn = document.querySelector(".prev");
+
+    const nextBtn = document.querySelector(".next");
+
+  prevBtn.addEventListener("click", function () {
+
+
+  });
+
+    nextBtn.addEventListener("click", function () {
+
+
+    });
+
+
+});
